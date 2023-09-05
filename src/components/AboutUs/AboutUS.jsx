00242b423932079c0730c8326d17a0bb8e1cde65
Amarasena B.H.L.M <amarasenabhlm.21@uom.lk>
2023-09-06 00:43:03 +0530

    updated
    update introduction

--- src/components/AboutUs/AboutUS.jsx
+++ src/components/AboutUs/AboutUS.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 
 import stylescontent from "./AboutUs.module.css";
+import image1 from "../AboutUs/aboutus1.png";
+import image2 from "../AboutUs/aboutus2.png";
+import image3 from "../AboutUs/aboutus3.png";
 
 export default function AboutUs() {
   return (
-    <div  className={stylescontent.topic1}>
-      <span style ={{ color : " white" } }> Innovation </span>
-      <span style={ { color : " purple" } }>  Meets </span>
-      <span style={{ color : " white" } }> Expertise </span>
+    <div>
+      <div className={stylescontent.topic1}>
+        <span style={{ color: " white" }}> Innovation </span>
+        <span style={{ color: " purple" }}> Meets </span>
+        <span style={{ color: " white" }}> Expertise </span>
+      </div>
+
+      <div>
+        <div>
+          <img className={stylescontent.img1} src={image1} alt="image1" />
+        </div>
+        <div>
+          <img className={stylescontent.img2} src={image2} alt="image2" />
+        </div>
+        <div>
+          <img className={stylescontent.img3} src={image3} alt="image3" />
+        </div>
+      </div>
     </div>
   );
 }
