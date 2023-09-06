0420cd81e59b399baed400bd7389e6c184f2e7ec
Kavindu Wijegunasekara <110486479+Kavinduprabasara@users.noreply.github.com>
2023-09-06 16:06:22 +0530

    Navbar-Step-1

---
+++ src/components/NavBar/NavLogo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from './Logo'
+
+function NavLogo() {
+  return (
+    <div className='border border-gray-500 rounded p-1'>
+        <Logo/>
+    </div>
+  )
+}
+
+export default NavLogo
