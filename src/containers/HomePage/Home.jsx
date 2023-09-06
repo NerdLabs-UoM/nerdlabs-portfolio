e2b8c183e64b76d425a2b8db8d178dab7f48dee5
Kavindu Wijegunasekara <110486479+Kavinduprabasara@users.noreply.github.com>
2023-09-06 17:05:39 +0530

    Update Home.jsx

--- src/containers/HomePage/Home.jsx
+++ src/containers/HomePage/Home.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
-
+import React from 'react'
 
 function Home() {
   return (
-    <div className = 'flex justify-between'>
-      
-    </div>
+    <div>Home</div>
   )
 }
 
