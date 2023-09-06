27f566a784556feff5059558c90fd90a44bb76fb
Amarasena B.H.L.M <amarasenabhlm.21@uom.lk>
2023-09-06 21:53:20 +0530

    Updated
    About Us

--- src/components/AboutUs/AboutUS.jsx
+++ src/components/AboutUs/AboutUS.jsx
@@ -15,14 +15,23 @@ export default function AboutUs() {
       </div>
 
       <div>
-        <div>
-          <img className={stylescontent.img1} src={image1} alt="image1" />
-        </div>
-        <div>
-          <img className={stylescontent.img2} src={image2} alt="image2" />
-        </div>
-        <div>
-          <img className={stylescontent.img3} src={image3} alt="image3" />
+        <p style={{ color:" violet"}}className={stylescontent.intro1}>
+          We specialize in delivering tailored IT solutions that drive growth,
+          efficiency, and digital transformation
+        </p>
+      </div>
+
+      <div>
+        <div className={stylescontent.images}>
+          <div>
+            <img className={stylescontent.img1} src={image1} alt="image1" />
+          </div>
+          <div>
+            <img className={stylescontent.img2} src={image2} alt="image2" />
+          </div>
+          <div>
+            <img className={stylescontent.img3} src={image3} alt="image3" />
+          </div>
         </div>
       </div>
     </div>
